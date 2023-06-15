b7df45f4c7666f5ffb2c1dd6b4923f0de4176c47
Luana Stela <luana.arantes.lsa@gmail.com>
2023-06-15 19:33:52 -0300

    Aluno aprovado

---
+++ src/Condicionais.java
@@ -0,0 +1,14 @@
+public class Condicionais {
+    public static void main(String[] args) {
+
+        int nota = 50;
+
+        // se nota maior ou igual a 70, então aprovado
+        // if-else
+        if (nota >= 70) {
+            System.out.println("Aluno aprovado!");
+        }else{
+            System.out.println("Não aprovado.");
+        }
+    }
+}
