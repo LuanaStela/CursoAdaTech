b20c7c17fabc0fa004e149482be2e5a3d10c0d7b
Luana Stela <luana.arantes.lsa@gmail.com>
2023-06-15 18:12:58 -0300

    Exercício com variáveis

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Olá, Luana!");
+    int a;
+    int b =2;
+    a = 3;
+    int soma = a + b;
+    int subtracao = a - b;
+    int multiplicacao = a * b;
+    float divisao = (float) a / b;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+
+
     }
 }
